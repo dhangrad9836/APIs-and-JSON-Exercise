141ce183a351a4ace472b26076d9829c3b1c5aa4
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-04-03 09:59:49 -0400

    Completed apis exercise

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
-    }
+        public static double GetTemperature(string apiCall)
+        {
+            var client = new HttpClient();
+
+            //this takes the apiCall key that is passed as a parameter to the method and Result is returned as a string
+            var response = client.GetStringAsync(apiCall).Result;
+
+            //store the temp that we first obtain from the response by passing the JSON into the JObject.Parse method..we then obtain the information from the fields ["main"]["temp"] which is then returned as a string from ToString(); note that if you put the api endpoint inside postman you can see the JSON and the breakdown of the the main -> temp
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+
+        } //end method
+    }// end Class
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,17 +1,16 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            //create RonVSKanye objects
+            //static methods of Kanye and Ron
             RonVSKanyeAPI.KanyeQuotes();
-
             RonVSKanyeAPI.RonSwanQuotes();
 
-
-
-
+            //Display Kanye and Ron having a conversation back and forth with a loop of 5 different conversations
 
             for (int i = 0; i < 5; i++)
             {
@@ -20,6 +19,30 @@
                 Console.WriteLine();
                 Console.WriteLine();
             }
+
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            Console.WriteLine("Weather Map");
+            //the File.ReadAllText() reads everything in the file and puts into a string
+            string key = File.ReadAllText("appsettings.json");
+
+            //store the APIKey temporarily from the appsettings file during runtime
+            //use our JObject from our Nuget Package to use the .GetValue() which pulls the value from our APIKey
+            //"APIKey" is what we named our key from inside the appsettings file
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            //ask user for their zip code location
+            Console.WriteLine("Please enter you zip code: ");
+            string zipCode = Console.ReadLine();
+
+            //setup apicall endpoint from openWeather app (this comes from openweather map) https://openweathermap.org/current#data
+            var apiCall =
+                $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
+
+
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {OpenWeatherMapAPI.GetTemperature(apiCall)} degrees F in your location.");
 
 
             Console.ReadLine();
